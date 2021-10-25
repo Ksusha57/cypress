dc15246533b0b2b45fcf1920412602c0f1660bb5
Ksusha57 <0939693496life@gmail.com>
2021-10-25 20:09:12 +0300

    remove some tests and add failed ones

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -17,7 +17,7 @@
  * @type {Cypress.PluginConfig}
  */
  const allureWriter = require('@shelex/cypress-allure-plugin/writer');
- // import allureWriter from "@shelex/cypress-allure-plugin/writer";
+//  import allureWriter from "@shelex/cypress-allure-plugin/writer";
  
  module.exports = (on, config) => {
      allureWriter(on, config);
